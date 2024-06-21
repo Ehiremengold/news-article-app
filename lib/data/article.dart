268c7156e5cf239f09dcedb70cee954da9a95aa1
Ehis <ehiremengold@gmail.com>
2024-06-21 10:28:10 +0100

    Body text handling

--- lib/data/article.dart
+++ lib/data/article.dart
@@ -31,10 +31,9 @@ class ArticleModel {
         subject: "Morocco Dominates Tanzania in Commanding AFCON 2023 Opener",
         body:
             """Favourites Morocco beat Tanzania 3-0 to open AFCON 2023 campaign with a statement victory. The Atlas Lions showcased their prowess from the outset, dominating possession and creating numerous scoring opportunities. 
-            
-            The first goal came early in the match, with Hakim Ziyech delivering a pinpoint cross to Youssef En-Nesyri, who headed it past the Tanzanian goalkeeper. Morocco continued to press, and their efforts paid off again just before halftime when Achraf Hakimi fired a powerful shot into the top corner.
-          In the second half, Morocco maintained their control and sealed the win with a third goal from Sofiane Boufal, who capitalized on a defensive error. Tanzania struggled to find their rhythm, unable to break through Morocco's solid defense.
-            With this convincing win, Morocco has set the tone for their AFCON 2023 campaign, sending a clear message to their rivals.""",
+            \n\nThe first goal came early in the match, with Hakim Ziyech delivering a pinpoint cross to Youssef En-Nesyri, who headed it past the Tanzanian goalkeeper. Morocco continued to press, and their efforts paid off again just before halftime when Achraf Hakimi fired a powerful shot into the top corner.
+          \n\nIn the second half, Morocco maintained their control and sealed the win with a third goal from Sofiane Boufal, who capitalized on a defensive error. Tanzania struggled to find their rhythm, unable to break through Morocco's solid defense.
+            \n\nWith this convincing win, Morocco has set the tone for their AFCON 2023 campaign, sending a clear message to their rivals.""",
         imagePath: 'assets/images/sport.webp',
         time: '1min ago'));
 
@@ -45,10 +44,10 @@ class ArticleModel {
             "Mental Trauma in a Warzone: Why Ukraine Needs Therapists More Than Ever",
         body: """
           Mental trauma in a warzone: Why Ukraine needs therapists. As the conflict in Ukraine continues, the psychological toll on its citizens is becoming increasingly apparent. The constant threat of violence, displacement, and loss has left deep scars on the mental health of many, creating an urgent need for professional psychological support.
-          Children, in particular, are bearing the brunt of this trauma. Exposure to the horrors of war at a young age can lead to long-lasting psychological issues, including PTSD, anxiety, and depression. Schools, once places of learning and safety, have been turned into shelters or remain closed, disrupting education and further impacting mental well-being.
-          Adults, too, are struggling to cope with the relentless stress and uncertainty. Many have lost loved ones, homes, and livelihoods, leading to widespread grief and hopelessness. The stigma surrounding mental health in Ukraine exacerbates the situation, as many are reluctant to seek help.
-          Therapists and mental health professionals are crucial in addressing this crisis. They provide essential support, helping individuals process their experiences, develop coping strategies, and rebuild their lives. However, the demand far exceeds the available resources, highlighting the need for international assistance and investment in mental health infrastructure.
-          As the conflict drags on, the psychological wounds deepen, making it imperative for Ukraine to prioritize mental health care. The healing process is long and complex, but with the right support, the resilience and strength of the Ukrainian people can prevail.
+          \n\nChildren, in particular, are bearing the brunt of this trauma. Exposure to the horrors of war at a young age can lead to long-lasting psychological issues, including PTSD, anxiety, and depression. Schools, once places of learning and safety, have been turned into shelters or remain closed, disrupting education and further impacting mental well-being.
+          \n\nAdults, too, are struggling to cope with the relentless stress and uncertainty. Many have lost loved ones, homes, and livelihoods, leading to widespread grief and hopelessness. The stigma surrounding mental health in Ukraine exacerbates the situation, as many are reluctant to seek help.
+          \n\nTherapists and mental health professionals are crucial in addressing this crisis. They provide essential support, helping individuals process their experiences, develop coping strategies, and rebuild their lives. However, the demand far exceeds the available resources, highlighting the need for international assistance and investment in mental health infrastructure.
+          \n\nAs the conflict drags on, the psychological wounds deepen, making it imperative for Ukraine to prioritize mental health care. The healing process is long and complex, but with the right support, the resilience and strength of the Ukrainian people can prevail.
                   """,
         imagePath: 'assets/images/war.webp',
         time: '4min ago'));
@@ -60,10 +59,10 @@ class ArticleModel {
             "New York Times Sues OpenAI, Microsoft for Infringing Copyrighted Works",
         body: """
               The New York Times sues OpenAI, Microsoft for infringing copyrighted works, marking a significant legal battle in the tech and media industries. The lawsuit alleges that OpenAI and Microsoft have used the newspaper’s content without permission in their AI training datasets, violating intellectual property rights.
-              The lawsuit claims that OpenAI's language models, which Microsoft integrates into its products, have generated content that closely mimics articles published by The New York Times. This unauthorized use of copyrighted material has raised concerns about the ethical and legal implications of AI technologies that rely on large-scale data scraping from online sources.
-              The New York Times asserts that this practice undermines the value of its journalism, which relies heavily on subscription revenue. By using its content to train AI without compensation or acknowledgment, OpenAI and Microsoft are accused of unfairly benefiting from the newspaper’s work.
-              This case is poised to set a precedent for how AI companies use copyrighted materials and could lead to stricter regulations and guidelines. The outcome may impact the broader tech industry, where the line between innovation and intellectual property rights continues to blur.
-              OpenAI and Microsoft have yet to respond publicly to the lawsuit. However, the implications of this legal battle extend beyond the courtroom, potentially reshaping the relationship between technology and media, and the future of AI development.
+              \n\nThe lawsuit claims that OpenAI's language models, which Microsoft integrates into its products, have generated content that closely mimics articles published by The New York Times. This unauthorized use of copyrighted material has raised concerns about the ethical and legal implications of AI technologies that rely on large-scale data scraping from online sources.
+              \n\nThe New York Times asserts that this practice undermines the value of its journalism, which relies heavily on subscription revenue. By using its content to train AI without compensation or acknowledgment, OpenAI and Microsoft are accused of unfairly benefiting from the newspaper’s work.
+              \n\nThis case is poised to set a precedent for how AI companies use copyrighted materials and could lead to stricter regulations and guidelines. The outcome may impact the broader tech industry, where the line between innovation and intellectual property rights continues to blur.
+              \n\nOpenAI and Microsoft have yet to respond publicly to the lawsuit. However, the implications of this legal battle extend beyond the courtroom, potentially reshaping the relationship between technology and media, and the future of AI development.
               """,
         imagePath: 'assets/images/tech.webp',
         time: '4min ago'));
@@ -75,10 +74,10 @@ class ArticleModel {
             "Flooding Submerges Refugee Camp in Northwestern Syria, Compounding Crisis",
         body: """
             Flooding submerges refugee camp in northwestern Syria, exacerbating the already dire humanitarian situation. Torrential rains have inundated the camp, home to thousands of displaced people who fled the ongoing conflict. Makeshift tents and shelters have been swept away, leaving families without basic necessities or protection from the elements.
-            The floodwaters have contaminated water supplies, increasing the risk of waterborne diseases. Sanitation facilities have been destroyed, making it difficult to maintain hygiene and prevent outbreaks. The already limited medical infrastructure is overwhelmed, struggling to address the urgent needs of the affected population.
-            Children and the elderly are particularly vulnerable in these conditions, facing heightened risks of hypothermia, dehydration, and illness. The flooding has also disrupted the distribution of aid, cutting off access to food, clean water, and medical supplies.
-            Humanitarian organizations are urgently calling for increased international assistance to address the immediate and long-term needs of the refugees. They emphasize the need for robust infrastructure to prevent future flooding and ensure the safety and well-being of the displaced population.
-            This natural disaster highlights the ongoing plight of refugees in Syria, who endure relentless hardship amid conflict and environmental challenges. The international community's response will be crucial in mitigating the impact of this crisis and providing hope to those most affected.
+            \n\nThe floodwaters have contaminated water supplies, increasing the risk of waterborne diseases. Sanitation facilities have been destroyed, making it difficult to maintain hygiene and prevent outbreaks. The already limited medical infrastructure is overwhelmed, struggling to address the urgent needs of the affected population.
+            \n\nChildren and the elderly are particularly vulnerable in these conditions, facing heightened risks of hypothermia, dehydration, and illness. The flooding has also disrupted the distribution of aid, cutting off access to food, clean water, and medical supplies.
+            \n\nHumanitarian organizations are urgently calling for increased international assistance to address the immediate and long-term needs of the refugees. They emphasize the need for robust infrastructure to prevent future flooding and ensure the safety and well-being of the displaced population.
+            \n\nThis natural disaster highlights the ongoing plight of refugees in Syria, who endure relentless hardship amid conflict and environmental challenges. The international community's response will be crucial in mitigating the impact of this crisis and providing hope to those most affected.
                     """,
         imagePath: 'assets/images/climate.webp',
         time: '29min ago'));
@@ -90,11 +89,11 @@ class ArticleModel {
             "Bafta Awards 2024: Oppenheimer, Poor Things, Barbie, and Saltburn Lead the Nominations",
         body: """
             Bafta Awards 2024: Oppenheimer, Poor Things, Barbie, and Saltburn get nominations, showcasing a diverse array of cinematic achievements. The nominations highlight the breadth of storytelling and creativity in this year's film industry, with these films standing out for their unique narratives, strong performances, and technical excellence.
-            Oppenheimer, directed by Christopher Nolan, has garnered attention for its gripping portrayal of the life of J. Robert Oppenheimer, the father of the atomic bomb. The film is praised for its intense storytelling, exceptional acting by Cillian Murphy, and Nolan's masterful direction, securing it several top nominations including Best Film and Best Director.
-            Poor Things, directed by Yorgos Lanthimos, is a visually stunning and darkly comedic exploration of identity and transformation. Emma Stone’s performance has been a standout, earning her a nomination for Best Actress. The film’s innovative approach to storytelling and its striking visual style have made it a favorite among critics and audiences alike.
-            Barbie, a surprising contender, directed by Greta Gerwig, has captivated audiences with its clever and satirical take on the iconic doll’s world. Margot Robbie’s portrayal of Barbie has earned her a nomination for Best Actress, while the film itself is up for Best Original Screenplay, reflecting its sharp and witty narrative.
-            Saltburn, directed by Emerald Fennell, is another notable nominee, with its dark, thrilling storyline and strong performances. The film's exploration of class and privilege, combined with Fennell’s distinctive directorial style, has secured it multiple nominations, including Best Film and Best Director.
-            As the Bafta Awards 2024 approaches, the excitement builds to see which of these outstanding films and talents will take home the prestigious awards.
+            \n\nOppenheimer, directed by Christopher Nolan, has garnered attention for its gripping portrayal of the life of J. Robert Oppenheimer, the father of the atomic bomb. The film is praised for its intense storytelling, exceptional acting by Cillian Murphy, and Nolan's masterful direction, securing it several top nominations including Best Film and Best Director.
+            \n\nPoor Things, directed by Yorgos Lanthimos, is a visually stunning and darkly comedic exploration of identity and transformation. Emma Stone’s performance has been a standout, earning her a nomination for Best Actress. The film’s innovative approach to storytelling and its striking visual style have made it a favorite among critics and audiences alike.
+            \n\nBarbie, a surprising contender, directed by Greta Gerwig, has captivated audiences with its clever and satirical take on the iconic doll’s world. Margot Robbie’s portrayal of Barbie has earned her a nomination for Best Actress, while the film itself is up for Best Original Screenplay, reflecting its sharp and witty narrative.
+            \n\nSaltburn, directed by Emerald Fennell, is another notable nominee, with its dark, thrilling storyline and strong performances. The film's exploration of class and privilege, combined with Fennell’s distinctive directorial style, has secured it multiple nominations, including Best Film and Best Director.
+            \n\nAs the Bafta Awards 2024 approaches, the excitement builds to see which of these outstanding films and talents will take home the prestigious awards.
         """,
         imagePath: 'assets/images/movie.webp',
         time: '1hr ago'));
